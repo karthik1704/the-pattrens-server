2fa9ddcdc00037a5c5cf089fc722d1bc34029a03
Karthik A <karthikthee7@gmail.com>
2021-01-13 21:46:54 +0530

    Add Platform And UiInages

--- ui_apps/models.py
+++ ui_apps/models.py
@@ -1,16 +1,31 @@
 from django.db import models
-from django.db.models.fields import CharField, DateTimeField
+from django.db.models.fields import CharField, DateTimeField, SlugField
 from django.db.models.fields.files import ImageField
 from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+class Platform(models.Model):
+    name = CharField(max_length=50)
+    slug = SlugField(unique=True)
+
+
 class UiApps(models.Model):
     name = CharField(max_length=100)
     created_at = DateTimeField(auto_now_add=True)
     modified_at = DateTimeField(auto_now=True) 
-    image = ImageField(upload_to='/apps/')
+    image = ImageField(upload_to='apps/')
+
+    class Meta:
+        verbose_name = 'User Interface Apps'
+        verbose_name_plural = 'User Interface Apps'
+
+
+    def __str__(self) -> str:
+        return self.name
 
 class UiImages(models.Model):
     uiapp = ForeignKey(UiApps, on_delete=models.CASCADE)
-    image = ImageField(upload_to='/apps/')
+    platform = ForeignKey(Platform, on_delete=models.DO_NOTHING)
+    image = ImageField(upload_to='apps/')
 
