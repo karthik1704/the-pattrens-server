55b7771596578ee33599b61bf8512b5da945069d
Karthik A <karthikthee7@gmail.com>
2021-01-13 21:46:09 +0530

    Add Contrils to  UIApps Admin

--- ui_apps/admin.py
+++ ui_apps/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import UiApps
 # Register your models here.
+class UiAPPSAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        model = UiApps
+        
+    list_display = ('name', 'created_at', 'modified_at')
+    fieldsets = (
+        (None, {'fields': ('name', 'image',)}),
+    )
+    readonly_fields = ['created_at', 'modified_at']
+
+admin.site.register(UiApps, UiAPPSAdmin)
