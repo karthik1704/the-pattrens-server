baaec2108565cd778345345d12080c8e8bd00881
Karthik A <karthikthee7@gmail.com>
2021-02-25 18:33:16 +0530

    Add Myboards urls

--- config/urls.py
+++ config/urls.py
@@ -20,9 +20,13 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('ui_apps.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/auth/', include('dj_rest_auth.urls')),
-    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
 
+    path('api/v1/', include('ui_apps.urls')),
+    path('api/v1/', include('myboards.urls')),
+    path('api/v1/', include('check.urls')),
+    
+    path('api/v1/auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/v1/auth/', include('dj_rest_auth.urls')),
+
+    path('api-auth/', include('rest_framework.urls')),
 ]
