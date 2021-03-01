96d91a23d1619ae9715ac85b7b16b3d1fab11a62
Karthik A <karthikthee7@gmail.com>
2021-03-01 11:25:30 +0530

    Add Username

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,12 +10,12 @@ class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'created', 'last_login')
     list_filter = ('is_superuser', 'is_staff')
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('username', 'email', 'password')}),
         ('Permissions', {'fields': ('is_superuser', 'is_staff', 'is_active')}),
     )
 
     add_fieldsets = (
-        (None, {'classes': ('wide',),'fields': ('email', 'password1','password2' )}),
+        (None, {'classes': ('wide',),'fields': ('username', 'email', 'password1','password2' )}),
     )
 
     search_fields = ('email',)
