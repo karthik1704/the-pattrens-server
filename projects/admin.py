a8398610a43977de1c01356f1fc961f242e219c1
Karthik A <karthikthee7@gmail.com>
2021-10-31 20:23:47 +0530

    add Projects to admin panel

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from projects.models import Platform, Project, Category, Element, Pattern,ProjectImage,ProjectVersion
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('pk','project_name', 'created_at', 'modified_at')
+    fieldsets = (
+        (None, {'fields': ('project_name', 'slug', 'url', 'copy_right', 'image',)}),
+        ('Category', {'fields': ( 'platform','category', )}),
+    )
+    readonly_fields = ['created_at', 'modified_at']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk','category_name', 'created_at', 'modified_at')
+
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = ('pk','platform_name', 'created_at', 'modified_at')
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Platform, PlatformAdmin)
+admin.site.register(Pattern)
+admin.site.register(Element)
+admin.site.register(ProjectVersion)
+admin.site.register(ProjectImage)
